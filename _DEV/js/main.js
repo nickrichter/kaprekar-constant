6903324448d5fe28ab2fb64bbcf1f5e0caae890e
Nicholos Richter <hello@nickrichter.com>
2022-07-19 22:17:06 -0500

    calculations
    main javascript functionality for handling digit check and calculating the valid input
    
    Signed-off-by: Nicholos Richter <hello@nickrichter.com>

--- _DEV/js/main.js
+++ _DEV/js/main.js
@@ -1,29 +1,68 @@
-function checkSubjectParameters(theSubject) {
-	let subjectSet = new Set(theSubject);
-	
-	if subjectSet.length > 1 {
-		return true;
-	}
-	else {
-		return false;
-	}
+let myInput = 2345;
+let calculatedValue = myInput;
+// This is what the "magic number" is, after which the result will always return 6174.
+let kaprekarsConstant = 6174;
+
+// console.log('the input', myInput);
+
+
+
+function checkSubjectUniqueDigits(theSubject) {
+	let subjectSet = new Set(theSubject.toString());
+	let setTestResult;
 	
 	console.log(subjectSet);
+	
+	if (subjectSet.size > 1) {
+		// console.log('pass');
+		setTestResult = 'pass';
+		return setTestResult;
+	}
+	else {
+		// console.log('false');
+		setTestResult = 'fail';
+		return setTestResult;
+	}
 }
 
-function kaprekarConstant() {
-	let theSubject = '1111';
+
+
+
+function kaprekarCalculation(myNumber) {
+	let theSubject = myNumber;
+	// let subjectCheckResult;
 	
-	checkSubjectParameters(theSubject);
+	// subjectCheckResult = checkSubjectUniqueDigits(theSubject);
 	
-	let subjectSmall = theSubject.split('');
-	let subjectlarge = Array.from(subjectSmall);
-	subjectSmall.sort();
-	subjectlarge.reverse();
+	// console.log('check result', subjectCheckResult);
 	
-	console.log(theSubject);
-	console.log(subjectSmall);
-	console.log(subjectlarge);
+	let subjectSmallArr = theSubject.toString().split('').sort();
+	let subjectLargeArr = Array.from(subjectSmallArr).reverse();
+	// subjectSmallArr.sort();
+	// subjectLargeArr.reverse();
+	
+	let subjectSmall = subjectSmallArr.join('');
+	let subjectLarge = subjectLargeArr.join('');
+	let newSubject = subjectLarge - subjectSmall;
+	
+	console.log('the subject', theSubject);
+	console.log('subject small', subjectSmall);
+	console.log('subject large', subjectLarge);
+	// console.log(newSubject);
+	
+	return newSubject;
 }
 
-kaprekarConstant();
+
+
+while(calculatedValue != kaprekarsConstant) {
+	if (checkSubjectUniqueDigits(calculatedValue) == 'pass') {
+		calculatedValue = kaprekarCalculation(calculatedValue);
+	}
+	else {
+		console.log('Input does not pass unique digits check');
+		break;
+	}
+	
+	console.log('new value', calculatedValue);
+}
